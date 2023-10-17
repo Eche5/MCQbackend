391bc9d2bf507a6cba7f0af1f0e2a59cf999e64d
Eche5 <111073744+Eche5@users.noreply.github.com>
2023-10-17 14:31:51 +0100

    protect route

--- Routes/resultRoute.js
+++ Routes/resultRoute.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const resultController = require("../Controller/resultController");
 const router = express.Router();
-// const verifyJWT = require("../Middlewares/verifyJWT");
+const verifyJWT = require("../Middlewares/verifyJWT");
 
-// router.use(verifyJWT);
+router.use(verifyJWT);
 
 router
   .route("/results")
